519de3b27987d0ed7c73a11b30d00bbbc08bbd0e
eoguryun1 <yun9702123@gmail.com>
2023-03-17 00:44:53 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add player");
+		System.out.println("2. Delete player");
+		System.out.println("3. Edit player");
+		System.out.println("4. View player");
+		System.out.println("5.Exit");
+
+	}
+
+}
